32374e063644d10ff6012e25223ced0c5693380a
panasiuk-site <107774012+IvanPanasiuk228@users.noreply.github.com>
2022-06-25 19:05:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const a = document.getElementById('h1')
+alert(a)
+
+const b = 'string'
+const c = 3
+const e = true
+
+const person = {
+  name: 'Ivan',
+  age: 33,
+}
+const person2 = Object.create(person)
+console.log(person2)
